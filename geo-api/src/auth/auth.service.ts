7a2aa146305396a185bf3f948e462a8208affb96
VictorCortez358 <00022120@uca.edu.sv>
2024-07-03 09:56:58 -0600

    Implemented JWT token in SignIn

--- geo-api/src/auth/auth.service.ts
+++ geo-api/src/auth/auth.service.ts
@@ -1,20 +1,27 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
 import { SignInDto } from './dto/sign-in-dto';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor (private usersService: UsersService){}
+    constructor (
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ){}
 
-    async SignIn(signIn: SignInDto): Promise<String>{
+    async SignIn(signIn: SignInDto): Promise<{ access_token: string }>{
         const user = await this.usersService.findOneUserByEmail(signIn.email);
         if(user?.password !== signIn.password){
             throw new UnauthorizedException()
         }
-        return "User signed in successfully";
+        
+        const payload = { email: user.email, sub: user.id }
+        return {
+            access_token: this.jwtService.sign(payload)
+        }
     }
 
     async SignUp(user: CreateUserDto): Promise<String>{
